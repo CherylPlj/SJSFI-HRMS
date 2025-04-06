cdcb26d44df514a0c307734869fd19211a680ffe
CherylPlj <139420219+CherylPlj@users.noreply.github.com>
2025-04-07 02:27:18 +0800

    Add files via upload
    Faculty Content COmponent

---
+++ FacultyContent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FacultyContent: React.FC = () => {
+  return (
+    <div className="text-black">
+      <h2>Faculty Content</h2>
+      <p>This is the content for the Faculty section.</p>
+    </div>
+  );
+};
+
+export default FacultyContent;
