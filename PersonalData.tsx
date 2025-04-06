0da81de12d142003caf70d2628bf27395c020ada
CherylPlj <139420219+CherylPlj@users.noreply.github.com>
2025-04-07 02:28:15 +0800

    Add files via upload
    Personal Data COmponent

---
+++ PersonalData.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PersonalData: React.FC = () => {
+    return (
+        <div className="text-black">
+            <h1>Personal Data</h1>
+            <p>This is the Personal Data component.</p>
+        </div>
+    );
+};
+
+export default PersonalData;
